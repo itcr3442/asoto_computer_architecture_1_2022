6ff19da47e5b53142a30b0676dc120e4d59b34aa
Fabian Montero <fabian@posixlycorrect.com>
2023-06-13 00:02:01 -0600

    proyecto_3: actualiza cpus. fast_cpu es WIP

--- proyecto_3/src/fast_cpu.py
+++ proyecto_3/src/fast_cpu.py
@@ -5,19 +5,28 @@ cpu = uarch.Cpu()
 try:
     while True:
         rip, insn = cpu.master.get_insn()
-        print(f"0x{rip:016x}: {insn}")
+
+        # solo imprimir nuestro programa
+        if (0x0000000000401080 <= rip < 0x00000000004011a0) or (0x0000000000401290 <= rip < 0x0000000000401d64):
+            print(f"0x{rip:016x}: {insn}")
 
         key = uarch.gen_search_str(*cpu.master.get_insn_info(insn))
 
         try:
             unit, latency = cpu.insns[key]
-            cpu.attempt(unit, latency)
+            if cpu.units.lock(unit):
+                cpu.cycles += latency
+            else:
+                cpu.wait_for(unit)
+
         except KeyError:
             print(f"Invalid instruction: {insn}")
             break
 
-        cpu.master.s()
+        if not cpu.master.s():
+            break
+
 finally:
     cpu.master.close()
     print("CPU without dynamic scheduler done.")
-    print(f"Execution took: {cpu.cycles} cycles")
+    print(f"Execution took: {cpu.cycles} cycles.")
