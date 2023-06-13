d928a4adb9f56839caff342ed6562be4c1f54d06
Fabian Montero <fabian@posixlycorrect.com>
2023-06-13 01:12:48 -0600

    proyecto_3: fast_cpu WIP

--- proyecto_3/src/fast_cpu.py
+++ proyecto_3/src/fast_cpu.py
@@ -5,12 +5,11 @@ cpu = uarch.Cpu()
 try:
     while True:
         rip, insn = cpu.master.get_insn()
+        key = uarch.gen_search_str(*cpu.master.get_insn_info(insn))
 
         # solo imprimir nuestro programa
         if (0x0000000000401080 <= rip < 0x00000000004011a0) or (0x0000000000401290 <= rip < 0x0000000000401d64):
             print(f"0x{rip:016x}: {insn}")
-
-        key = uarch.gen_search_str(*cpu.master.get_insn_info(insn))
 
         try:
             unit, latency = cpu.insns[key]
@@ -18,7 +17,6 @@ try:
                 cpu.cycles += latency
             else:
                 cpu.wait_for(unit)
-
         except KeyError:
             print(f"Invalid instruction: {insn}")
             break
